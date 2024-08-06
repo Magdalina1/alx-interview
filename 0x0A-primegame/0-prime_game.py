be7568432e0513e1d40684592097eb32293b1714
Magdalina1 <majdalol95@gmail.com>
2024-08-06 01:59:57 -0700

    fix the code :)

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -5,25 +5,34 @@ def isWinner(x, nums):
     """function that checks for the winner"""
     if not nums or x < 1:
         return None
+    
     max_num = max(nums)
-
     my_filter = [True for _ in range(max(max_num + 1, 2))]
-    for i in range(2, int(pow(max_num, 0.5)) + 1):
-        if not my_filter[i]:
-            continue
-        for j in range(i * i, max_num + 1, i):
-            my_filter[j] = False
-    my_filter[0] = my_filter[1] = False
-    y = 0
+    
+    # Implementing Sieve of Eratosthenes
+    for i in range(2, int(max_num ** 0.5) + 1):
+        if my_filter[i]:
+            for j in range(i * i, max_num + 1, i):
+                my_filter[j] = False
+    
+    my_filter[0] = my_filter[1] = False  # 0 and 1 are not primes
+    count_primes = 0
+    
+    # Counting cumulative primes up to each index
     for i in range(len(my_filter)):
         if my_filter[i]:
-            y += 1
-        my_filter[i] = y
-    player1 = 0
-    for x in nums:
-        player1 += my_filter[x] % 2 == 1
-    if player1 * 2 == len(nums):
+            count_primes += 1
+        my_filter[i] = count_primes
+    
+    player1_wins = 0
+    
+    for n in nums:
+        if my_filter[n] % 2 == 1:
+            player1_wins += 1
+    
+    if player1_wins * 2 == len(nums):
         return None
-    if player1 * 2 > len(nums):
+    elif player1_wins * 2 > len(nums):
         return "Maria"
-    return "Ben"
+    else:
+        return "Ben"
